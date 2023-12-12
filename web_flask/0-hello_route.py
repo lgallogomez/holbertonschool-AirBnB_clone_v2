d2e7a75e912bc5845877f86776464a4e3e1e529f
lgallogomez <l.gallogomez@gmail.com>
2023-12-12 13:28:01 -0500

    changed port to 0.0.0.0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,3 +10,6 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False) # a la funcion hello_world le añadimos el metodo route del objeto app de clase Flask con argumentos especifico para usarlo
 def hello_world():  
     return("<p>Hello HBNB!</p>")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
