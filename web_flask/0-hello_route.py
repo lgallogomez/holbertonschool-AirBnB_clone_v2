de3e4c23111749fa535db0af4479f2babad22b50
lgallogomez <l.gallogomez@gmail.com>
2023-12-12 13:42:30 -0500

    changed port to 0.0.0.0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,13 +3,18 @@
 '''
 module creates an object of class Flask and returns str
 '''
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False) # a la funcion hello_world le añadimos el metodo route del objeto app de clase Flask con argumentos especifico para usarlo
-def hello_world():  
-    return("<p>Hello HBNB!</p>")
+
+@app.route("/", strict_slashes=False) 
+def hello_world():
+    """
+    A la funcion hello_world le añadimos el metodo route del objeto app de clase Flask con argumentos especifico para usarlo
+    """
+    return("Hello HBNB!")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
