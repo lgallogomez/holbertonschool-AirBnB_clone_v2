76e64293e81abcd5945a6be12e4cf9c05ec66300
lgallogomez <l.gallogomez@gmail.com>
2023-12-12 17:00:10 -0500

    added default value to route /python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+'''
+module creates an object of class Flask and returns str
+'''
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """
+    funcion usa metodo route de app
+    """
+    return("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    funcion usa metodo route de app
+    """
+    return("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def print_text(text):
+    """
+    funcion usa metodo route de app
+    """
+    replaced_text = text.replace("_", " ")
+    return(f"C {escape(replaced_text)}")
+
+
+@app.route("/python/", defaults= {"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def print_text_python(text='is cool'):
+    """
+    funcion usa metodo route de app
+    """
+    replaced_text = text.replace("_", " ")
+    return(f"Python {escape(replaced_text)}")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
