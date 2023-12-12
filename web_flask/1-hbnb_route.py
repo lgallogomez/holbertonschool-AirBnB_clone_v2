6a70cc7c401a9828f17a10c52984e4364f09769e
lgallogomez <l.gallogomez@gmail.com>
2023-12-12 14:15:38 -0500

    moved 1- file inside folder

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+'''
+module creates an object of class Flask and returns str
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """
+    funcion usa metodo route de app
+    """
+    return("Hello HBNB!")
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    funcion usa metodo route de app
+    """
+    return("HBNB")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
